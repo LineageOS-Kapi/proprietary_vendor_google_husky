aea4917b3195531baa99421bb9083f8bb0f5f572
kylian <kylianalex@e.email>
2026-06-17 22:10:57 +0200

    Remove AppDirectedSMSService (Verizon bloat)

--- husky-vendor.mk
+++ husky-vendor.mk
@@ -588,7 +588,6 @@ PRODUCT_PACKAGES += \
     vendor.google.whitechapel.audio.audioext@4.0 \
     vendor.google.whitechapel.audio.extension-V7-ndk \
     AICorePrebuilt-aicore_20250130.00_RC01 \
-    AppDirectedSMSService \
     CarrierSettings \
     CarrierWifi \
     ConnMO \
